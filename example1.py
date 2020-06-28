61e3d732a14a2d6b6ce3bf462f10ef8b5b4a7a8c
Hassan Adam <haskhr@hotmail.com>
2020-06-28 20:58:06 +0400

    xml parsing

--- example1.py
+++ example1.py
@@ -1,10 +1,19 @@
-n= input("Please enter your name :")
-print("Hello"+" "+n) 
-print ( " this is " + n)
-#trial test
-<<<<<<< HEAD
-# another merging test for 3 way merge
-=======
-# to check the branches 
-# 
->>>>>>> newfeature1
+import xml.etree.ElementTree as ET
+# get the xml file data
+stream = open('sample.xml','r')
+
+#Parse the data into ElementTree object
+xml=ET.parse(stream)
+
+#Get the 'root' Element object from the ElementTree 
+root=xml.getroot()
+
+
+# Iterate through each child of the root Element 
+for e in root:
+    #print the stringfield version of the element
+    print(ET.tostring(e))
+    print("")
+
+    #print the 'Id' Attribute of tje Element object
+    print(e.get("Id"))
